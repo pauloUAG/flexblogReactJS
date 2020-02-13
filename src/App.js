5518f163043823626036b55b8ab7725fdb9ac2db
paulo <paulosousa1011@gmail.com>
2020-02-13 17:45:17 -0300

    Termino da primeira secao do main

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import Header from './components/header';
 import Menu from './components/menu';
+import Main from './pages/main'
 
 function App() {
   return (
     <BrowserRouter>
     <Header />
     <Menu />
+    <Main />
     </BrowserRouter>
   );
 }
---
+++ src/pages/main/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.css'
+
+const Main = () => {
+    return (
+        <section className="valores">
+            <h1>Novos Valores e<br/> Propriedades de CSS</h1>
+        </section>
+    );
+}
+
+export default Main;
