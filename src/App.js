b8b14e2aba154385faee3402afebc0ab5b0b90c0
paulo <paulosousa1011@gmail.com>
2020-02-13 14:52:02 -0300

    Adicao de novos componentes e termino do Header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
-import Header from './components/Header/index'
+import Header from './components/header';
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Header />
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+    <Header />
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/header/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => {
+    return (
+        <div className="Header">
+            <div className="container">
+                <div className="header-info">
+                    Seg / Sex - 08:00 às 18:00
+                </div>
+                <div className="header-info">
+                    +55 87 98169-2110
+                </div>
+                <div className="header-info">
+                    Rua 04, Vale do Mundau
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
