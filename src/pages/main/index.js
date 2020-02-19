378f9288aa1901dc99df9938c913b3491e5f122b
paulo <paulosousa1011@gmail.com>
2020-02-18 22:47:49 -0300

    Seçao Sobra parcialmente completa e secao produto tambem parcialmente completa

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,10 +1,47 @@
 import React from 'react'
 import './styles.css'
+import samurai from '../../assets/samurax'
 
 const Main = () => {
     return (
-        <section className="valores">
-            <h1>Novos Valores e<br/> Propriedades de CSS</h1>
+        <section className="section-um">
+            <div className="valores">
+                <h1>Novos Valores e<br/> Propriedades de CSS</h1>
+            </div>
+            <div className="sobre-container">
+                <div className="sobre-info">
+                    <h1>Sobre</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a eleifend magna. 
+                    Nunc ac tempor nulla. Suspendisse sollicitudin maximus augue. Phasellus feugiat 
+                    magna vitae mi aliquam, eget placerat arcu volutpat. Phasellus pulvinar justo id 
+                    dignissim semper. Sed nisi ligula, ultricies non.
+                    </p>
+                    <br/>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a eleifend magna. 
+                    justo id dignissim semper.
+                    </p>
+                </div>
+                <div className="sobre-imagens">
+                    <img src={samurai} alt="samuraiUm" />
+                </div>
+                {/* <div className="sobre-imagens-2">
+                    <img src={samurai} alt="samuraiDois"/>
+                </div> */}
+            </div>
+            <div className="produtos-container">
+                <h1>Produtos</h1>
+                <div className="produtos">
+                    <div className="produto-item white">
+                        <h2>White</h2>
+                    </div>
+                    <div className="produto-item black">
+                        <h2>Black</h2>
+                    </div>
+                    <div className="produto-item purple">
+                        <h2>Purple</h2>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
